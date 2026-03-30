92666a519dd0fae7f960318bcb155e821592b984
RoddyRodriguez7519938 <rjr7519938@lpssonline.com>
2026-03-30 18:01:31 +0000

    todo 7 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -45,7 +45,17 @@ var init = function (window) {
 
         // TODO 7 : Use a loop to create multiple circles
 
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
 
+         for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
+
+            drawCircle();
+
+        }
 
 
         ///////////////////
